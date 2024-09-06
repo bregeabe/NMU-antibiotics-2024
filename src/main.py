951fa95fa62e42dc2b804bc3a9077fbec7cc5c02
abe brege <bregeabe@gmail.com>
2024-09-06 09:47:12 -0400

    got starter window

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+root.title("Antibiotic Report")
+root.minsize(900, 750)  # width, height
+root.maxsize(1000, 800)
+root.geometry("300x300+50+50")  # width x height + x + y
+root.mainloop()
