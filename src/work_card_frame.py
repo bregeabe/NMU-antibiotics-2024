858db19817aa2a341cdcea7cbcc23018911643e9
Billy <biblock@nmu.edu>
2024-11-08 11:39:53 -0500

    added basic layout

--- src/work_card_frame.py
+++ src/work_card_frame.py
@@ -12,3 +12,28 @@ class Work_Card_Frame:
         # Clear any existing widgets in the dashboard
         self.main_screen.clear_frame()
         print("displaying workcard")
+
+        #Configure the grid
+
+        # Build each section of the work card
+    
+    def create_title(self):
+        pass
+
+    def create_prefilled_section(self):
+        pass
+
+    def create_non_prefilled_section(self):
+        pass
+
+    def create_culture_id(self):
+        pass
+
+    def create_priority(self):
+        pass
+
+    def create_direct_gram_stain(self):
+        pass
+
+    def create_button_section(self):
+        pass
